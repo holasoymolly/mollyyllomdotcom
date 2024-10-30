663ba38163546f6534282258fe3a222d6418206f
Molly Yllom <hola@mollyyllom.com>
2024-10-30 19:19:39 -0400

    Ajuste Margenes Texto Projects Page

--- src/pageComponents/ProjectsPage/ProjectsPage.tsx
+++ src/pageComponents/ProjectsPage/ProjectsPage.tsx
@@ -11,12 +11,14 @@ export const ProjectsPage: FC = () => {
     <div>
       <Header />
       <main className="px-0 py-12 space-y-16">
-        <section className="max-w-full flex flex-col md:flex-row md:space-x-8 items-start px-4 lg:px-12">
+        <section className="max-w-full flex flex-col md:flex-row md:space-x-8 px-4 lg:px-12">
           <div className="w-full md:w-1/2">
-            <h1 className="text-4xl font-bold text-indigo-950 ml-4">Proyectos</h1>
+            <h1 className="text-4xl font-bold text-indigo-950 ml-12 sm:ml-12 md:ml-12 lg:ml-4 mb-4 sm:mb-6">
+              Proyectos
+            </h1>
           </div>
-          <div className="w-full md:w-1/2">
-            <p className="text-lg leading-relaxed text-indigo-950">
+          <div className="w-full md:w-1/2 pl-4 md:pl-8">
+            <p className="text-lg leading-relaxed text-indigo-950 ml-9 sm:ml-8 md:ml-0 -mb-8 sm:mb-4 pr-4 sm:pr-6">
               ¡Bienvenidx a la galería de proyectos! Aquí encontrarás algunos de los trabajos en los que he dejado mi huella creativa. Cada proyecto cuenta una historia única que espero te inspire y te motive.
             </p>
           </div>
