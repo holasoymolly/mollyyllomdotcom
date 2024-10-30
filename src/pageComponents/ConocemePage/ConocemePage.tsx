81165464639515f81a9543d01dcc3d5414615334
Molly Yllom <hola@mollyyllom.com>
2024-10-30 19:06:04 -0400

    Correciones en Conoceme Page

--- src/pageComponents/ConocemePage/ConocemePage.tsx
+++ src/pageComponents/ConocemePage/ConocemePage.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -11,60 +11,65 @@ export const ConocemePage: FC = () => {
     <div>
       <Header />
 
-      <main className="py-11 space-y-1">
+      <main className="py-11">
         {/* Introductory Bio Section */}
         <section className="max-w-full flex flex-col md:flex-row md:space-x-8 px-4 lg:px-12">
-          <div className="w-full md:w-1/2">
-            <h1 className="text-4xl font-bold text-indigo-950 ">
-              Conóceme
-            </h1>
-          </div>
-          <div className="w-full md:w-1/2">
-            <p className="text-lg leading-relaxed text-indigo-950">
-              Con más de 15 años de experiencia en diseño gráfico y dirección de arte, mi pasión es
-              crear historias visuales que conecten con las audiencias y dejen una impresión duradera.
-              He liderado y colaborado con equipos talentosos, estrechamente con clientes y socios
-              externos para garantizar la ejecución exitosa de estrategias creativas y campañas
-              publicitarias. Mi enfoque es desarrollar soluciones creativas que impactan, con un ojo
-              siempre puesto en los detalles que hacen la diferencia.
-            </p>
+        <div className="w-full md:w-1/2">
+  <h1 className="text-4xl font-bold text-indigo-950 ml-12 sm:ml-12 md:ml-12 lg:ml-4 mb-4 sm:mb-6">
+    Conóceme
+  </h1>
+</div>
+          <div className="w-full md:w-1/2 pl-4 md:pl-8">
+          <p className="text-lg leading-relaxed text-indigo-950 ml-9 sm:ml-8 md:ml-0 mb-4 sm:mb-6 pr-4 sm:pr-6">
+  Con más de 15 años de experiencia en diseño gráfico y dirección de arte, mi pasión es
+  crear historias visuales que conecten con las audiencias y dejen una impresión duradera.
+  He liderado y colaborado con equipos talentosos, estrechamente con clientes y socios
+  externos para garantizar la ejecución exitosa de estrategias creativas y campañas
+  publicitarias. Mi enfoque es desarrollar soluciones creativas que impactan, con un ojo
+  siempre puesto en los detalles que hacen la diferencia.
+</p>
           </div>
         </section>
 
         {/* Image and Call to Action Section */}
-        <section className="flex flex-col md:flex-row items-center md:items-stretch md:py-16 mb-0">
-          <div className="w-screen md:w-1/2 h-[41.47rem] overflow-hidden">
+        <section className="flex flex-col md:flex-row items-center md:items-center md:py-10 mb-0 lg:mb-0 xl:mb-0 2xl:mb-0">
+          <div className="w-full md:w-1/2 h-[30rem] sm:h-[35rem] md:h-[30rem] lg:h-[35rem] xl:h-[40rem] overflow-hidden flex items-center">
             <img
               src="/img/molly/molly2.webp"
               alt="Molly Yllom"
               className="object-cover w-full h-full"
+              style={{
+                objectPosition: 'center 20%',
+              }}
             />
           </div>
-          <div className="w-full md:w-1/2 flex flex-col justify-center p-6 md:p-12 bg-stone-200 space-y-6 text-center md:text-left">
-            <h2 className="text-2xl sm:text-3xl md:text-2xl lg:text-4xl xl:text-5xl font-bold text-indigo-950">
-              ¿Listx para llevar tu proyecto al siguiente nivel?
-            </h2>
-            <p className="text-lg leading-relaxed text-indigo-950">
-              En el mundo del diseño gráfico, cada decisión cuenta. Desde la elección de colores hasta
-              la tipografía utilizada, cada detalle comunica un mensaje único sobre tu marca. Con mi{" "}
-              <strong>asesoría personalizada</strong>, te guiaré en cada paso del proceso para que el diseño
-              de tu marca sea inolvidable.
-            </p>
-            <div className="mt-6">
-              <Button
-                href="https://calendly.com/hola-msny/30min"
-                text="Haz tu cita aquí"
-                size="large"
-                className="px-4 py-2 text-sm md:px-6 md:py-3 md:text-base lg:px-8 lg:py-4 lg:text-lg"
-                variant="dark"
-              />
+          <div className="w-full md:w-1/2 flex flex-col justify-center p-6 md:p-10 bg-stone-200 space-y-4 md:space-y-6 text-center">
+            <div className="mx-auto md:mx-0">
+              <h2 className="text-2xl sm:text-3xl md:text-2xl lg:text-4xl xl:text-5xl font-bold text-indigo-950">
+                ¿Listx para llevar tu proyecto al siguiente nivel?
+              </h2>
+              <p className="text-lg leading-relaxed text-indigo-950">
+                En el mundo del diseño gráfico, cada decisión cuenta. Desde la elección de colores hasta
+                la tipografía utilizada, cada detalle comunica un mensaje único sobre tu marca. Con mi{" "}
+                <strong>asesoría personalizada</strong>, te guiaré en cada paso del proceso para que el diseño
+                de tu marca sea inolvidable.
+              </p>
+              <div className="mt-4 md:mt-6">
+                <Button
+                  href="https://calendly.com/hola-msny/30min"
+                  text="Haz tu cita aquí"
+                  size="large"
+                  className="px-4 py-2 text-sm md:px-6 md:py-3 md:text-base lg:px-8 lg:py-4 lg:text-lg"
+                  variant="dark"
+                />
+              </div>
             </div>
           </div>
         </section>
       </main>
 
       {/* Quote Banner Section */}
-      <section style={{ marginTop: "-110px" }}>
+      <section className="-mt-6 md:-mt-21 lg:-mt-21 xl:-mt-21 2xl:-mt-21">
         <QuoteBanner />
       </section>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,26 @@ const config: Config = {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+        '-14': '-3.5rem',
+        '-16': '-4rem',
+        '-20': '-5rem',
+        '-21': '-5.25rem', // Valor ajustado entre -20 y -22
+        '-22': '-5.5rem',
+        '-24': '-6rem',
+        '-28': '-7rem',
+        '-32': '-8rem',
+        '-40': '-10rem',
+        '-48': '-12rem',
+        '-52': '-13rem',
+        '-64': '-16rem',
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
