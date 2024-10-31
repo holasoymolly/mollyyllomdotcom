5f77c3ff0fe3c8903135ff6feec50444b21bb97b
Molly Yllom <hola@mollyyllom.com>
2024-10-31 15:39:16 -0400

    Ajustes en el MediaSection: aumento de tamaño de título y ajuste de la imagen para mejorar la visualización

--- src/pageComponents/HomePage/components/MediaSection.tsx
+++ src/pageComponents/HomePage/components/MediaSection.tsx
@@ -5,19 +5,22 @@ import { Button } from "@/components/Button";
 export const MediaSection = () => {
   return (
     <section className="flex flex-col md:flex-row items-center md:py-16 max-h-[600px] overflow-hidden">
-      <figure className="w-full lg:w-1/2 h-full overflow-hidden">
+      <figure className="w-full md:w-[50%] lg:w-[45%] xl:w-[40%] h-[40rem] overflow-hidden flex items-center"> {/* Ajuste de ancho */}
         <img
           src="/img/molly/molly1.webp"
           alt="Media Image"
           className="object-cover w-full h-full"
+          style={{
+            objectPosition: 'center 5%', // Posición más hacia la derecha
+          }}
         />
       </figure>
-      <div className="w-full h-full md:w-1/2 flex flex-col items-center md:items-start justify-between p-6 md:p-12 space-y-4 md:space-y-6 text-center md:text-left">
-        <h2 className="text-2xl sm:text-3xl md:text-2xl lg:text-4xl xl:text-5xl font-bold text-indigo-950 mobile-padding tablet-padding">
+      <div className="w-full md:w-[50%] lg:w-[55%] xl:w-[60%] flex flex-col items-center md:items-start justify-between p-6 md:p-12 lg:p-16 space-y-4 md:space-y-6 text-center md:text-left">
+        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-indigo-950 leading-tight">
           ¿Listx para llevar tu 
           <span className="block sm:inline"> proyecto al siguiente nivel?</span>
         </h2>
-        <p className="text-base sm:text-lg md:text-base lg:text-xl xl:text-2xl text-indigo-950 mobile-padding tablet-padding">
+        <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-indigo-950">
           En el mundo del diseño gráfico, cada decisión cuenta. Desde la elección de colores hasta
           la tipografía utilizada, cada detalle comunica un mensaje único sobre tu marca.
         </p>
