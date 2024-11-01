6f8c0ad59f18e17c4c92cbc1b9938516b436f963
Molly Yllom <hola@mollyyllom.com>
2024-11-01 16:21:25 -0400

    Agregue hover al PortfolioGrid

--- src/components/PortfolioGrid/PortfolioGrid.tsx
+++ src/components/PortfolioGrid/PortfolioGrid.tsx
@@ -7,13 +7,23 @@ export const PortfolioGrid = () => {
     <section>
       <div className="grid grid-cols-3 gap-0">
         {activeProjects.map((item, index) => (
-          <figure key={index} className="w-full aspect-square overflow-hidden">
+          <figure key={index} className="w-full aspect-square overflow-hidden relative group">
             {item.slug ? (
               <a href={`/proyectos/${item.slug}`} rel="noreferrer">
                 <img src={item.portfolioImage} alt={item.title} className="object-cover w-full h-full block" />
+                <div className="absolute inset-0 bg-violet-900 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <span className="text-white text-lg md:text-xl font-bold">{item.title}</span>
+                </div>
               </a>
             ) : (
-              <img src={item.portfolioImage} alt={item.title}  className="object-cover w-full h-full block" />
+              <>
+                <img src={item.portfolioImage} alt={item.title} className="object-cover w-full h-full block" />
+                <div className="absolute inset-0 bg-violet-900 opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
+                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <span className="text-white text-lg md:text-xl font-bold">{item.title}</span>
+                </div>
+              </>
             )}
           </figure>
         ))}
