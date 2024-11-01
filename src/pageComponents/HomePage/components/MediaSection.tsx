6840aa8d53053a492a75860dcf68f2539c5720b0
Molly Yllom <hola@mollyyllom.com>
2024-11-01 13:37:21 -0400

    Actualizacion a MediaSection en el crop de la Foto

--- src/pageComponents/HomePage/components/MediaSection.tsx
+++ src/pageComponents/HomePage/components/MediaSection.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/Button";
 export const MediaSection = () => {
   return (
     <section className="flex flex-col md:flex-row items-center md:py-16 max-h-[600px] overflow-hidden">
-      <figure className="w-full md:w-[50%] lg:w-[45%] xl:w-[40%] h-[40rem] overflow-hidden flex items-center"> {/* Ajuste de ancho */}
+      <figure className="w-full md:w-[65%] lg:w-[78%] xl:w-[78%] 2xl:w-[80%] h-[40rem] overflow-hidden flex items-center justify-center md:justify-end pr-4 lg:pr-8">
         <img
           src="/img/molly/molly1.webp"
           alt="Media Image"
@@ -15,7 +15,7 @@ export const MediaSection = () => {
           }}
         />
       </figure>
-      <div className="w-full md:w-[50%] lg:w-[55%] xl:w-[60%] flex flex-col items-center md:items-start justify-between p-6 md:p-12 lg:p-16 space-y-4 md:space-y-6 text-center md:text-left">
+      <div className="w-full md:w-[55%] lg:w-[60%] xl:w-[65%] 2xl:w-[70%] flex flex-col items-center md:items-start justify-between p-6 md:p-12 lg:p-16 space-y-4 md:space-y-6 text-center md:text-left">
         <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-indigo-950 leading-tight">
           ¿Listx para llevar tu 
           <span className="block sm:inline"> proyecto al siguiente nivel?</span>
