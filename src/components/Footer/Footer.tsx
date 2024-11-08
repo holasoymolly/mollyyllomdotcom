6b2f3bb5ea0cb95db377e92a1fe7c6294d2bc44d
Molly Yllom <hola@mollyyllom.com>
2024-11-08 16:19:50 -0400

    Corrección Error Vercel Footer 2

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -8,18 +8,17 @@ import { RiBehanceFill } from '@/icons/RiBehanceFill';
 import { RiInstagramLine } from '@/icons/RiInstagramLine';
 
 export const Footer = () => {
-  const [logoIndex, setLogoIndex] = useState(0);
-  const logos = [
-    "/img/logo/molly-yllom-logo-homepage.webp",
-    "/img/logo/molly-yllom-logo-homepage-2.webp"
-  ];
+  const [logoSrc, setLogoSrc] = useState("/img/logo/molly-yllom-logo-homepage.webp");
 
   useEffect(() => {
-    // Solo ejecuta el cambio en el lado del cliente
     if (typeof window !== 'undefined') {
       const timer = setInterval(() => {
-        setLogoIndex((current) => (current === 0 ? 1 : 0));
-      }, 1000); // Cambia cada 1 segundo
+        setLogoSrc(current =>
+          current === "/img/logo/molly-yllom-logo-homepage.webp"
+            ? "/img/logo/molly-yllom-logo-homepage-2.webp"
+            : "/img/logo/molly-yllom-logo-homepage.webp"
+        );
+      }, 1000); // Change image every 1 second
       return () => clearInterval(timer);
     }
   }, []);
@@ -27,6 +26,7 @@ export const Footer = () => {
   return (
     <footer className="w-full py-6 bg-stone-200">
       <div className="flex flex-col items-center px-4 md:px-8 md:flex-row md:justify-between">
+        {/* Navigation Menu */}
         <nav className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 text-center md:text-left md:flex-1">
           <Link href="/conoceme/" className="text-indigo-950 font-bold hover:text-violet-900">Conóceme</Link>
           <Link href="/proyectos" className="text-indigo-950 font-bold hover:text-violet-900">Proyectos</Link>
@@ -34,10 +34,11 @@ export const Footer = () => {
           <Link href="/descargas" className="text-indigo-950 font-bold hover:text-violet-900">Descargas</Link>
         </nav>
 
+        {/* Logo that changes automatically */}
         <div className="mt-8 mb-4 md:mt-0 md:mb-0 lg:mt-0 lg:mb-0 md:flex-none flex justify-center">
           <Link href="/">
             <Image
-              src={logos[logoIndex]}
+              src={logoSrc}
               alt="Molly Yllom"
               width={137}
               height={88}
@@ -48,6 +49,7 @@ export const Footer = () => {
           </Link>
         </div>
 
+        {/* Social Media Icons */}
         <div className="flex space-x-4 mt-6 md:mt-0 md:flex-1 justify-center md:justify-end">
           <Link href="https://www.instagram.com/holasoymolly" aria-label="Instagram" className="transform transition-transform duration-200 hover:scale-110">
             <RiInstagramLine className="w-6 h-6 text-indigo-950 hover:text-violet-900" />
