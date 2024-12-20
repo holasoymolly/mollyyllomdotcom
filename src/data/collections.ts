af4aa0ecdb3f00eba62809fb7bc86e1ddbdf736c
holasoymolly <hola@mollyyllom.com>
2024-12-20 19:04:37 -0400

    agregué una nueva colección a los NFTs

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -1,5 +1,22 @@
 export const collections = [
     {
+      name: "Pixeled Flexies Vol. 2",
+      link: "https://launchmynft.io/sol/12117",
+      images: [
+        "/img/nft/pixeled-flexies-vol-2/0.webp",
+        "/img/nft/pixeled-flexies-vol-2/1.webp",
+        "/img/nft/pixeled-flexies-vol-2/18.webp",
+        "/img/nft/pixeled-flexies-vol-2/21.webp",
+        "/img/nft/pixeled-flexies-vol-2/25.webp",
+        "/img/nft/pixeled-flexies-vol-2/27.webp",
+        "/img/nft/pixeled-flexies-vol-2/28.webp",
+        "/img/nft/pixeled-flexies-vol-2/29.webp",
+        "/img/nft/pixeled-flexies-vol-2/31.webp",
+        "/img/nft/pixeled-flexies-vol-2/37.webp",
+      ],
+    },
+    
+    {
       name: "Pixeled Flexies",
       link: "https://launchmynft.io/sol/11391",
       images: [
@@ -15,4 +32,5 @@ export const collections = [
         "/img/nft/pixeled-flexies/pixeled-flexies-10.webp",
       ],
     },
+
   ];
